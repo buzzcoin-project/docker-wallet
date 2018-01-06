030e84ef9b9fcd7727ebffee2f023ee3c9d0c5ab
CryptoCarl <trevor.benson+cryptocarl@gmail.com>
2018-01-06 10:55:53 -0800

    build.sh and branch updates
    Branch now builds stabilit branch. build.sh is setup for _branch naming of containers. Since --link is still used to link the wallet to the firstnode we can only support one testnet at a time (per docker host). Once I replace this with a customer docker network then testing multiple branches per docker host will be possible.

--- testnet/branch/build.sh
+++ testnet/branch/build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 cd ./firstnode
-docker build . -t firstnode_master
-docker run -d --name firstnode_master firstnode_master
+docker build . -t firstnode_branch
+docker run -d --name firstnode_branch firstnode_branch
 echo "BUILD OF FIRSTNODE COMPLETE, CHECKING RUNNING CONTAINERS:"
 docker ps
 
 cd ../wallet
-docker build . -t wallet_master
+docker build . -t wallet_branch
 echo "BUILD OF  WALLET COMPLETE, CHECKING RUNNING CONTAINERS:"
-docker run -d -P --name wallet_master --link firstnode_master wallet_master
+docker run -d -P --name wallet_branch --link firstnode_branch wallet_branch
 
 # IF YOU WANT TO CONNECT TO ONE OF THE CONTAINERS CLI TO CHECK ON THINGS USE THE FOLLOWING COMMANDS
 # TO DISCONECT FROM THE CONTAINER AFTER YOU CONNECT WITHOUT KILLING IT USE <CTRL-P> <CTRL-Q>
-# docker exec -it wallet_master /bin/bash or docker exec -it firstnode_master /bin/bash
+# docker exec -it wallet_branch /bin/bash or docker exec -it firstnode_branch /bin/bash
